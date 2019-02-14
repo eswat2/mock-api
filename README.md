79e1a6a2fa58a8608e6bad5187bb5c9d8dfba51f
Richard Hess <eswat2@gmail.com>
2019-02-14 14:29:36 -0800

    simple json-server

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mock-api
+
+```
+npx json-server --watch src/db.json --port 5555
+```
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+const PORT = 5555
+
+server.use(middlewares)
+server.use(router)
+server.listen(PORT, () => {
+  console.log('JSON Server is running on PORT', PORT)
+})
